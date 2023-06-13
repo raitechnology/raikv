332e2b70f2430ab6c3a1e4b17cc5929111e3c4d0
Chris Anderson <chris@raitechnology.com>
2023-06-13 14:31:07 -0700

    update zipf

--- include/raikv/zipf.h
+++ include/raikv/zipf.h
@@ -5,53 +5,78 @@
 namespace rai {
 namespace kv {
 
-/* more or less a port of ycsb/generator/ZipfianGenerator.java */
-template <int zipf, int zipd, class RNG> /* zip const = zipf / zipd */
-struct ZipfianGen {
-  const double theta     = (double) zipf / (double) zipd;
-  const double alpha     = 1.0 / ( 1.0 - theta );
-  const double baseplus1 = 1.0 + pow( 0.5, theta );
-
-  uint64_t next_val[ 16 ];
-  size_t   next_cnt;
-  RNG    & r;
+struct ZipfianConst {
+  double   theta;
+  double   alpha;
+  double   baseplus1;
   uint64_t item_cnt;
   double   zetan,
            eta;
 
-  ZipfianGen( uint64_t cnt, RNG & rng )
-      : next_cnt( 0 ), r( rng ), item_cnt( cnt ) {
-    double zeta2_theta = zeta_static( 2 );
-    this->zetan = zeta_static( cnt );
-    this->eta   = ( 1 - pow( 2.0 / (double) cnt, 1 - theta ) ) /
-                  ( 1 - zeta2_theta / this->zetan );
-  }
-
-  double zeta_static( uint64_t n ) {
+  static double zeta_static( double theta,  uint64_t n ) {
     double sum = 0;
     for ( uint64_t i = 0; i < n; i++ )
       sum += 1.0 / pow( (double) ( i + 1 ), theta );
     return sum;
   }
 
+  ZipfianConst( int zipf,  int zipd,  uint64_t cnt ) {
+    this->theta     = (double) zipf / (double) zipd;
+    this->alpha     = 1.0 / ( 1.0 - theta );
+    this->baseplus1 = 1.0 + pow( 0.5, theta );
+    this->item_cnt  = cnt;
+
+    double zeta2_theta = zeta_static( this->theta, 2 );
+    this->zetan = zeta_static( this->theta, cnt );
+    this->eta   = ( 1 - pow( 2.0 / (double) cnt, 1 - this->theta ) ) /
+                  ( 1 - zeta2_theta / this->zetan );
+  }
+};
+
+struct ZipfianBuf {
+  const ZipfianConst & zconst;
+  static const size_t VAL_BUFSIZE = 16;
+  uint64_t next_val[ VAL_BUFSIZE ];
+  size_t   next_cnt;
+
+  ZipfianBuf( const ZipfianConst &c ) : zconst( c ), next_cnt( 0 ) {}
+
+  void gen( const double *rvals ) {
+    do {
+      double u  = *rvals++;
+      double uz = u * this->zconst.zetan;
+      uint64_t v;
+      if ( uz < 1.0 )
+        v = 0;
+      else if ( uz < this->zconst.baseplus1 )
+        v = 1;
+      else
+        v = (uint64_t) ( this->zconst.item_cnt *
+          pow( this->zconst.eta * u - this->zconst.eta + 1, this->zconst.alpha ) );
+      this->next_val[ this->next_cnt++ ] = v;
+    } while ( this->next_cnt < VAL_BUFSIZE );
+  }
+};
+
+/* more or less a port of ycsb/generator/ZipfianGenerator.java */
+template <int zipf, int zipd, class RNG> /* zip const = zipf / zipd */
+struct ZipfianGen {
+  const ZipfianConst zconst;
+  RNG        & r;
+  ZipfianBuf   zip;
+
+  ZipfianGen( uint64_t cnt, RNG & rng )
+      : zconst( zipf, zipd, cnt ), r( rng ), zip( this->zconst ) {}
+
   uint64_t next( void ) {
-    if ( this->next_cnt == 0 ) {
-      do {
-        double u  = this->r.next_double();
-        double uz = u * this->zetan;
-        uint64_t v;
-        if ( uz < 1.0 )
-          v = 0;
-        else if ( uz < baseplus1 )
-          v = 1;
-        else
-          v = (uint64_t) ( this->item_cnt *
-                 pow( this->eta * u - this->eta + 1, alpha ) );
-        this->next_val[ this->next_cnt++ ] = v;
-      } while ( this->next_cnt < sizeof( this->next_val ) /
-                                 sizeof( this->next_val[ 0 ] ) );
+    for (;;) {
+      if ( this->zip.next_cnt > 0 )
+        return this->zip.next_val[ --this->zip.next_cnt ];
+      double rvals[ ZipfianBuf::VAL_BUFSIZE ];
+      for ( size_t i = 0; i < ZipfianBuf::VAL_BUFSIZE; i++ )
+        rvals[ i ] = this->r.next_double();
+      this->zip.gen( rvals );
     }
-    return this->next_val[ --this->next_cnt ];
   }
 };
 
